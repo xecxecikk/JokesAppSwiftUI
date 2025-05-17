80cd5e4788ff6850716a2c4704afc3490c1eb3b6
xecxecikk <xecekrdnz_94@outlook.com>
2025-05-17 20:07:58 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokesAppSwiftUI
+//
+//  Created by XECE on 17.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by XECE on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
